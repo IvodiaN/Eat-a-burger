2b3daa2fab7aa0b143491c6715750af63ce9c3cd
IvodiaN <ivodiainjoh@gmail.com>
2021-04-10 10:59:21 -0400

    Updated main branch

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 // Pull in required dependencies
 var express = require('express');
-var bodyParser = require('body-parser');
+
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 var app = express();
 
 // Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
